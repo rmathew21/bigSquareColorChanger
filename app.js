3f6f1ad1e4cd9c1025190caec440af53fc85db97
rob <robmathew21@gmail.com>
2025-01-07 09:44:34 -0600

    added eventlistener for keydown and added fn makeRandomColor, added styles to center content

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const h1 = document.querySelector('.h1Title');
 const button = document.querySelector('.btn');
 
 button.addEventListener('click', () => {
-    
+    const newColor = makeRandomColor()
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+})
+
+button.addEventListener('keydown', () => {
+    if (e.key === 'space') {
+        const newColor = makeRandomColor()
+        document.body.style.backgroundColor = newColor
+        h1.innerText = newColor
+    }
+})
+const makeRandomColor = () => {
     const r = Math.floor(Math.random() * 255)
     const g = Math.floor(Math.random() * 255)
     const b = Math.floor(Math.random() * 255)
     
-    
-    const newColor = `rgb(${r}, ${g}, ${b})`
-    document.body.style.backgroundColor = newColor;
-
-    h1.innerText = newColor;
-})
+    return `rgb(${r}, ${g}, ${b})`
+}
 
